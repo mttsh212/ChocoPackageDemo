d29eadcc8ac0c86524f93d100973bf9d7f5e5507
Muhammad Tayyab Tahir <muhammadtayyab.tahir.ext@siemens-healthineers.com>
2024-08-12 13:12:00 +0200

    ,

--- ConsoleApp1/chocolateyinstall.ps1
+++ ConsoleApp1/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-Binfile -Name ConsoleApp1 -Path "$toolsDir\\MyApp.exe"
+Install-Binfile -Name ConsoleApp1 -Path "$toolsDir\\ConsoleApp1.exe"
